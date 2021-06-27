de75376beee9113dc79b857355904687e0cca4cd
Finrod_Amandil <severin.zahler@gmail.com>
2021-06-27 16:13:03 +0200

    Zwischenresultate für Romber-Extrapolation

--- 7_2_6_Romberg-Extrapolation.py
+++ 7_2_6_Romberg-Extrapolation.py
@@ -1,22 +1,41 @@
 import numpy as np
 
 
-def Tf(f, a, b, n):
-    xi = np.array([a + i * ((b - a) / n) for i in range(1, n)], dtype=np.float64)
+def Tf(f, a, b, n, j):
+    print("     Startwert a = " + str(a))
+    print("       Endwert b = " + str(b))
+    print("Anzahl Scritte n = " + str(n))
+
     h = (b - a) / n
-    return h * ((f(a) + f(b)) / 2 + np.sum(f(xi)))
+
+    print("  Schrittweite h = (b - a) / n = " + str(h))
+
+    xi = np.array([a + i * ((b - a) / n) for i in range(1, n)], dtype=np.float64)
+
+    print("xi = " + str(xi))
+
+    T = h * ((f(a) + f(b)) / 2 + np.sum(f(xi)))
+
+    print("T{}0 = h * ((f(a) + f(b)) / 2 + SUM(f(xi))) = {}\n".format(j, T))
+
+    return T
 
 
 def romberg_extrapolate(f, a, b, m):
+    print("1. Berechne die Tj0 mit der Trapezregel:")
+    print("----------------------------------------")
     T = np.zeros((m + 1, m + 1), dtype=np.float64)
 
-    T[0:, 0] = [Tf(f, a, b, 2 ** j) for j in range(m + 1)]
+    T[0:, 0] = [Tf(f, a, b, 2 ** j, j) for j in range(m + 1)]
 
+    print("2. Berechne die Tjk aus den Tj0:")
+    print("--------------------------------")
     for k in range(1, m + 1):
         for j in range(m + 1 - k):
             T[j, k] = (4**k * T[j + 1, k - 1] - T[j, k - 1]) / (4**k - 1)
+            print("T{}{} = (4^{} * T{}{} - T{}{}) / (4^{} - 1) = {}".format(j, k, k, j + 1, k - 1, j, k - 1, k, T[j, k]))
 
-    print('Tij = \n', T)
+    print('\nTij = \n', T)
     return T[0, m]
 
 
@@ -30,4 +49,4 @@ m = 4
 
 T = romberg_extrapolate(f, a, b, m)
 
-print('\nIntegral mit Romber-Extrapolation: ', T)
+print('\nIntegral mit Romberg-Extrapolation: ', T)
